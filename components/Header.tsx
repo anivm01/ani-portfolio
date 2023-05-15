958d9d1289ac4934943b9e97b6ba8e724db3d2c5
anivm01 <anivm.webdev@gmail.com>
2023-05-15 16:25:10 -0400

    add mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
     return (
         <header className="max-w-screen-xl mx-auto">
             <nav className="flex justify-between items-center p-4">
                 <Link href="/"><Image width={100} height={50} src="/icons/logo.svg" alt="site logo" /></Link>
-                <ul className="hidden md:flex gap-x-4 justify-end p-4 items-baseline">
+                <button className="md:hidden" type="button" onClick={() => setMenuIsOpen(current => !current)}>
+                    <Image src="/icons/hamburger.svg" alt="hamburger menu icon" height={30} width={30} />
+                </button>
+                <ul className=
+                    // "hidden md:flex gap-x-4 justify-end p-4 items-baseline"
+                    {`${menuIsOpen ? "absolute top-12 right-0 flex flex-col gap-4 bg-almost-white" : "hidden"} md:flex gap-x-4 justify-end p-4 items-baseline`}
+                    onClick={() => setMenuIsOpen(false)}
+                >
                     <li>
                         <Link className="p-4 text-ani-indigo" href="/">Home</Link>
                     </li>
@@ -23,7 +33,6 @@ const Header = () => {
                         <a className="p-4 text-ani-indigo flex gap-4 items-baseline" href="/resume/ani-mihaylova-resume.pdf" download><span>Resume</span><Image width={"20"} height={"20"} src="/icons/download.svg" alt="download icon" /></a>
                     </li>
                 </ul>
-                <Image className="md:hidden" src="/icons/hamburger.svg" alt="hamburger menu icon" height={30} width={30} />
             </nav>
         </header>
     );
